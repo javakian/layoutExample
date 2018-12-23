eca3b52d62d3335162052897acf5bd0a439b023e
Chris Hawkins <556267+chh51@users.noreply.github.com>
2018-12-22 17:23:49 -0700

    Better feedback on Stories toolbar

--- ex04-Editor/ex04-Editor/StoriesViewToolbar.swift
+++ ex04-Editor/ex04-Editor/StoriesViewToolbar.swift
@@ -9,8 +9,7 @@ import Interface
 internal final class StoriesViewToolbar: UIToolbar {
     let switchMode        = UISwitch(frame: CGRect(x: 0, y: 0, width: 38, height: 38))
     let barItemMode       = UIBarButtonItem()
-    let barItemHelp       = UIBarButtonItem(title: "About", style: .plain , target: self,
-                                            action: #selector(_toolbarActionHelp(sender_:)))
+    let barItemHelp       = UIBarButtonItem()
     let barItemSpace      = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil )
     var touchTip:         LongTouchTip?     = nil
     weak var controller:  UIViewController? = nil
@@ -22,6 +21,13 @@ internal final class StoriesViewToolbar: UIToolbar {
             barItemMode.customView = self.switchMode
             switchMode.addTarget(self, action: #selector(_modeSwitchEvent(sender:forEvent:)),
                                  for: UIControl.Event.valueChanged )
+        }
+        if ( barItemHelp.image == nil ) {
+            barItemHelp.image     = UIImage(named: "59-info-symbol")
+            barItemHelp.action    = #selector( _toolbarActionHelp(sender_:) )
+            barItemHelp.target    = self
+            barItemHelp.style     = .plain
+            barItemHelp.tintColor = UIColor.blue
         }
         if ( touchTip == nil ) {
             touchTip = LongTouchTip(view: self )
@@ -42,10 +48,19 @@ internal final class StoriesViewToolbar: UIToolbar {
 
     // MARK: UIBarButtonItem actions
     @objc private func _toolbarActionHelp( sender_: UIBarButtonItem ) {
+        self.touchTip!.show()
     }
     @objc private func _modeSwitchEvent( sender: UISwitch, forEvent event_: UIEvent ) {
         let newState = StateManager.singleton.getStateBool( .isEditMode_Bool ) ? false : true
         _ = StateManager.singleton.setState( .isEditMode_Bool, bool: newState )
+        let message = newState ? "Edit Mode" : "View Mode"
+        let easyTip = EasyTipView(text: message, preferences: LongTouchTip.tipViewPreferences, delegate: nil )
+        easyTip.show(forView: sender )
+        sender.isEnabled = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0 ) {
+            easyTip.dismiss()
+            sender.isEnabled = true
+        }
         self._toolbarModeSet()
     }
     private func _toolbarModeSet() {
